be60071196e90d11748f7f7b0246ffb678b315e9
fillipedem <fillipedem@gmail.com>
2019-06-04 20:37:38 -0300

    Spells info file

---
+++ spells_info.py
@@ -0,0 +1,29 @@
+import json
+
+spells = {}
+
+# lendo informação sobre os campeões
+with open("./data/summoner_spell_info.json") as f:
+    spells_info = json.load(f)
+
+for it in spells_info['data']:
+    info = spells_info['data'][it]
+    
+    spells[info['id']] = {}
+    spells[info['id']]['name'] = info['name']
+    spells[info['id']]['id'] = info['id']
+
+    
+def get_spell(spell_id):
+    
+    return spells[spell_id]['name']
+
+def all_spells_names():
+    """
+    Return all spells
+    """
+    return ['Teleport', 'Cleanse', 'Ghost', 
+            'Barrier', 'Exhaust', 'Ignite',
+           'Smite', 'Heal', 'Flash']
+
+    
